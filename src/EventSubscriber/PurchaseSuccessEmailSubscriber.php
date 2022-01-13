12b9f1eedaf81e247a618e1f33e71fa7dc3f1f05
Nicolas marcaud <nmarcau@gmail.com>
2022-01-13 10:27:33 +0100

    [MAILER] Ajout de mailer et envoi d'un mail de confirmation de commande

--- src/EventSubscriber/PurchaseSuccessEmailSubscriber.php
+++ src/EventSubscriber/PurchaseSuccessEmailSubscriber.php
@@ -2,18 +2,28 @@
 
 namespace App\EventSubscriber;
 
-use App\Event\PurchaseSuccessEvent;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Mime\Email;
+use App\Event\PurchaseSuccessEvent;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Security;
 
 class PurchaseSuccessEmailSubscriber implements EventSubscriberInterface
 {
 
     protected $logger;
+    protected $mailer;
+    protected $security;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, MailerInterface $mailer, Security $security)
     {
         $this->logger = $logger;
+        $this->mailer = $mailer;
+        $this->security = $security;
     }
 
     // Exprime la configuration
@@ -27,7 +37,33 @@ class PurchaseSuccessEmailSubscriber implements EventSubscriberInterface
 
     public function sendSuccessEmail(PurchaseSuccessEvent $purchaseSuccesEvent)
     {
-        $this->logger->info("Email envoyé pour la commande n°" . $purchaseSuccesEvent->getPurchase()->getId() );
+        /** @var User */
+        $currentUser = $this->security->getUser();
+
+         /** @var Purchase */
+        $purchase = $purchaseSuccesEvent->getPurchase();
+
+        // Créer l'email pour le customer
+        $email = new TemplatedEmail();
+        $email
+            ->from(new Address("contact@aureliefabre.com", "Institut Aurélie Fabre")) // Objet Address avec mail et nom
+            ->to(new Address($currentUser->getEmail(), $currentUser->getFirstName() . ' ' . $currentUser->getLastName()))
+            ->text("La commande n°" . $purchase->getId() . " vient d'être validée")
+            
+            // Nom du 'template' twig
+            ->htmlTemplate('emails/purchase_customer_confirmation.html.twig')   
+
+            // Tableau associatif de variables à lui passer 
+            ->context([
+                'user' => $currentUser,
+                'purchase' => $purchase
+            ])
+
+            ->subject("Votre commande n°" . $purchase->getId() . ", a bien été confirmée");
+
+        $this->mailer->send($email);
+
+        $this->logger->info("Email envoyé pour la commande n°" . $purchase->getId() );
     }
 
 }
